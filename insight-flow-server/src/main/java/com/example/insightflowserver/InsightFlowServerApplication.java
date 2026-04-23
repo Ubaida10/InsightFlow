42b60c5bcb22fc37ad9b13429d1793b9162dd7f4
Muhammad Abu Ubaida Aljerah <123815303+Ubaida10@users.noreply.github.com>
2026-04-23 14:14:43 +0500

    Created project setup

---
+++ insight-flow-server/src/main/java/com/example/insightflowserver/InsightFlowServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.insightflowserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightFlowServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsightFlowServerApplication.class, args);
+    }
+
+}
---
+++ insight-flow-server/src/test/java/com/example/insightflowserver/InsightFlowServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insightflowserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightFlowServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
